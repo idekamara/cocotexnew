74d5d703f755286df09f4b8ac70a008a8d88ec00
idekamara <idekamara@gmail.com>
2020-01-07 15:04:47 +0000

    Correct error on heroku

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function ()
 //Auth::routes(['verify'=>true]);
 //route page d'accueil
 Route::get('/welcome', 'HomeController@index')->name('welcome');
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 //Route::get('/home', 'HomeController@index')->name('home');
 //routes pour les differentes operations sur les produits
 Route::get('/products/index','ProductsController@index')->name('products.index');
